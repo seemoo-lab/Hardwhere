dfca919d2aaf2d8195d905382bca02c09cb6d2c3
Aron Heinecke <aheinecke@seemoo.tu-darmstadt.de>
2020-01-28 14:24:43 +0100

    Add custom asset text view
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

---
+++ app/src/main/java/com/heinecke/aron/LARS/ui/editor/AssetTextView.kt
@@ -0,0 +1,20 @@
+package com.heinecke.aron.LARS.ui.editor
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.heinecke.aron.LARS.R
+
+class AssetTextView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+    init {
+        View.inflate(context, R.layout.asset_text_view_text, this)
+
+        val label: TextView = findViewById(R.id.asset_text_view_label)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.AssetTextView)
+        label.text = attributes.getText(R.styleable.AssetTextView_label)
+        attributes.recycle()
+    }
+}
