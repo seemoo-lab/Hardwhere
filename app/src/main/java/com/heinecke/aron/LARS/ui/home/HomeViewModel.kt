5dd95a38061444f3fd8d37a44ee2395503362c84
Aron Heinecke <aheinecke@seemoo.tu-darmstadt.de>
2020-02-06 18:23:24 +0100

    Add build date & commit to home screen
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- app/src/main/java/com/heinecke/aron/LARS/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/heinecke/aron/LARS/ui/home/HomeViewModel.kt
@@ -3,11 +3,12 @@ package com.heinecke.aron.LARS.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.heinecke.aron.LARS.BuildConfig
 
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Build: ${BuildConfig.BUILD_TIME} \nCommit: ${BuildConfig.GitHash}"
     }
     val text: LiveData<String> = _text
 }
