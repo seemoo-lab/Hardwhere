c9ea89753554b86d93f82472a5e96193bfc701cd
Aron Heinecke <aheinecke@seemoo.tu-darmstadt.de>
2021-04-11 13:32:36 +0200

    Add sentry to about
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- app/src/main/java/de/tu_darmstadt/seemoo/LARS/ui/about/AboutViewModel.kt
+++ app/src/main/java/de/tu_darmstadt/seemoo/LARS/ui/about/AboutViewModel.kt
@@ -8,7 +8,7 @@ import de.tu_darmstadt.seemoo.LARS.BuildConfig
 class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Large Accessories Retrieval System\nBuild: ${BuildConfig.BUILD_TIME} \nCommit: ${BuildConfig.GitHash}\n\nCopyright SEEMOO.\nUsing the following libraries copyright by their authors:\nrxjava\nretrofit\nokhttp\ngoogle material designs\nicons from flaticon.com"
+        value = "Large Accessories Retrieval System\nBuild: ${BuildConfig.BUILD_TIME} \nCommit: ${BuildConfig.GitHash}\n\nCopyright SEEMOO.\nUsing the following libraries copyright by their authors:\nrxjava\nretrofit\nokhttp\ngoogle material designs\nsentry\nicons from flaticon.com"
     }
     val text: LiveData<String> = _text
 }
