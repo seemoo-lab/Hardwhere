4acffbe605d8a8199657a1258858cf8454d46508
Aron Heinecke <aheinecke@seemoo.tu-darmstadt.de>
2021-04-11 11:44:07 +0200

    Add fragment navigation to sentry
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- app/src/main/java/de/tu_darmstadt/seemoo/LARS/MainActivity.kt
+++ app/src/main/java/de/tu_darmstadt/seemoo/LARS/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -30,6 +31,7 @@ import de.tu_darmstadt.seemoo.LARS.data.APIInterface
 import de.tu_darmstadt.seemoo.LARS.data.model.Selectable.User
 import de.tu_darmstadt.seemoo.LARS.data.model.UserData
 import de.tu_darmstadt.seemoo.LARS.ui.login.LoginActivity
+import io.sentry.core.Breadcrumb
 import io.sentry.core.Sentry
 import retrofit2.Call
 import retrofit2.Callback
@@ -62,6 +64,12 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+        navController.addOnDestinationChangedListener { _controller,destination,_bundle ->
+            val breadcrumb = Breadcrumb("Fragment Navigation")
+            breadcrumb.setData("fragment","$destination")
+            breadcrumb.category = "ui.lifecycle"
+            Sentry.addBreadcrumb(breadcrumb)
+        }
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
