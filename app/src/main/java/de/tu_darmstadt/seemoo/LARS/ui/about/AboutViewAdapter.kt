bff937bf7481b2307af0eaaf5000638c80d426a9
Aron Heinecke <aheinecke@seemoo.tu-darmstadt.de>
2021-04-29 22:42:41 +0200

    Implement proper about sections for libs
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

---
+++ app/src/main/java/de/tu_darmstadt/seemoo/LARS/ui/about/AboutViewAdapter.kt
@@ -0,0 +1,76 @@
+package de.tu_darmstadt.seemoo.LARS.ui.about
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import de.tu_darmstadt.seemoo.LARS.R
+import de.tu_darmstadt.seemoo.LARS.data.model.Asset
+
+class AboutViewAdapter(
+    private val mListener: OnListInteractionListener?,
+    private val aboutList: ArrayList<AboutFragment.About>
+)         :
+    RecyclerView.Adapter<AboutViewAdapter.ViewHolder>() {
+
+    // holder class to hold reference
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        //get view reference
+        var name: TextView = view.findViewById(R.id.about_item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // create view holder to hold reference
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.recycler_about_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    /**
+     * Replace elements with new ones
+     */
+    fun replaceElements(newData: List<AboutFragment.About>) {
+        this.aboutList.clear()
+        this.aboutList.addAll(newData)
+        this.notifyDataSetChanged()
+    }
+
+    fun getItemAt(id: Int): AboutFragment.About {
+        return aboutList[id]
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val entry = aboutList[position]
+        holder.name.text = entry.name
+
+        with(holder.view) {
+            tag = entry
+            setOnClickListener {
+                mListener?.onListItemClicked(entry)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return aboutList.size
+    }
+
+    fun removeItem(position: Int) {
+        this.aboutList.removeAt(position)
+        this.notifyItemRemoved(position)
+    }
+
+    /**
+     * Interface for recyclerview listeners
+     */
+    interface OnListInteractionListener {
+        fun onListItemClicked(item: AboutFragment.About)
+    }
+}
--- app/src/main/java/de/tu_darmstadt/seemoo/LARS/ui/about/AboutViewModel.kt
+++ app/src/main/java/de/tu_darmstadt/seemoo/LARS/ui/about/AboutViewModel.kt
@@ -1,14 +1,74 @@
 package de.tu_darmstadt.seemoo.LARS.ui.about
 
+import android.content.Context
+import android.icu.text.DateFormat.getDateTimeInstance
+import android.icu.text.SimpleDateFormat
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import de.tu_darmstadt.seemoo.LARS.BuildConfig
+import de.tu_darmstadt.seemoo.LARS.R
+import io.sentry.core.Sentry
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.stream.Collectors
 
 class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Large Accessories Retrieval System\nBuild: ${BuildConfig.BUILD_TIME} \nCommit: ${BuildConfig.GitHash}\n\nCopyright SEEMOO.\nUsing the following libraries copyright by their authors:\nrxjava\nretrofit\nokhttp\ngoogle material designs\nsentry\nicons from flaticon.com"
+        val date: String = getDateTimeInstance().format(BuildConfig.BUILD_TIME)
+
+        value = "Large Accessories Retrieval System, copyright SEEMOO\nBuild: $date \nCommit: ${BuildConfig.GitHash}\nIncluded libraries & licenses:"
     }
     val text: LiveData<String> = _text
+    private val _libList: MutableLiveData<ArrayList<AboutFragment.About>> = MutableLiveData(arrayListOf())
+    val libList: LiveData<ArrayList<AboutFragment.About>> = _libList
+
+    val _currentLicenseText: MutableLiveData<String?> = MutableLiveData()
+    val currentLicenseText: LiveData<String?> = _currentLicenseText
+    fun resetLicenseText() {
+        _currentLicenseText.postValue(null)
+    }
+    fun loadData(context: Context) {
+        if(!_libList.value.isNullOrEmpty()) {
+            return
+        }
+        viewModelScope.launch {
+            try {
+                val buffer = BufferedReader(InputStreamReader(context.resources.openRawResource(R.raw.licenses)))
+                val list = buffer.lines().map { line ->
+                    val split = line.split(',')
+                    AboutFragment.About(split[0], resolveLicense(split[1]))
+                }.collect(Collectors.toList())
+                Log.d(this@AboutViewModel::class.java.name,"Loaded $list")
+                _libList.postValue(list as ArrayList<AboutFragment.About>?)
+            } catch (e: Exception) {
+                Log.wtf("Unable to load about resources",e)
+                Sentry.captureException(e)
+            }
+        }
+    }
+
+    fun loadLicense(context: Context, resource: Int) {
+        viewModelScope.launch {
+            try {
+                val buffer = BufferedReader(InputStreamReader(context.resources.openRawResource(resource)))
+                _currentLicenseText.postValue(buffer.readText())
+            } catch (e: Exception) {
+                Log.wtf("Unable to load license",e)
+                Sentry.captureException(e)
+            }
+        }
+    }
+
+    private fun resolveLicense(input: String): Int {
+        return when(input.trim()) {
+            "apache2" -> R.raw.apache2
+            "sentry" -> R.raw.sentry
+            else -> throw Exception("Unknown license resource $input")
+        }
+    }
 }
